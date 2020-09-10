5830fa3870e57effd5e970e6d268d5e02bddf130
Mariusz Perzynski <m.perzyk@interia.pl>
2020-09-10 11:11:41 +0200

    update css html and add arrow

---
+++ main.js
@@ -0,0 +1,20 @@
+const ImageSlider = [{
+    img: "images/bee.jpg",
+    text:"LIGHT"
+},
+{
+    img: "images/Wakacje.jpg",
+    text:"VACATION"
+},
+{
+    img: "images/gryl.jpg",
+    text:"GRILL"
+},
+{
+    img: "images/dom.jpg",
+    text:"HOUSE"
+},
+{
+    img: "images/zboze.jpeg",
+    text:"GRAIN"
+}]
